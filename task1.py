a094997a8275333346c3d088cf19aabd889358bf
Kaboom154 <owen.riley08@gmail.com>
2026-05-29 09:26:10 -0700

    w

---
+++ task1.py
@@ -0,0 +1,9 @@
+'''
+##### Task 1
+Create the user interface described in the image task1.png.
+You should use only the .pack() or .grid() methods
+(2 points) 
+'''
+
+import tkinter as tk
+from tkinter import *
